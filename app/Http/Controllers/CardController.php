1f51b54934b49c7acb187ae09bd1d7a77847d797
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-11-28 13:51:10 +0900

    テーブル作成、リレーション作成

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Card;
+
+class CardController extends Controller
+{
+    //
+}
---
+++ database/migrations/2022_11_29_120952_create_points_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('card_id')->constrained();
+            $table->integer('point_charge');
+            $table->string('point_expiration',50);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+};
