411add60eaf55b55f8c876846022117dbd4af4f4
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-11-29 17:45:06 +0900

    index作成

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Model\Card;
+use App\Models\Card;
+use Illuminate\Support\Facades\Auth;
+
 
 class CardController extends Controller
 {
-    //
+     public function index(Card $card)
+     {
+         return view('cards/index')->with(['cards'=>$card->get()]);
+     }
+     
+      public function create(Card $card)
+     {
+         return view('cards/index')->with(['cards'=>$card->get()]);
+     }
 }
--- database/migrations/2022_11_28_124538_create_cards_table.php
+++ database/migrations/2022_11_28_124538_create_cards_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
             $table->string('name',50);
-            $table->string('image_path');
-            $table->string('barcode_path');
+            $table->string('image_path')->nullable();
+            $table->string('barcode_path')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
