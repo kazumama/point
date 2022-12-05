fd33c7b0f042272d899fb7fba2252e1aefc83f9a
kazumama <kazuma.ishida.kosuke@icloud.com>
2022-12-05 11:15:33 +0900

    show.blade作成

---
+++ resources/views/cards/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cards</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+       <h1>
+            {{ $card->image_path }}
+        </h1>
+        <h2>
+                {{ $card->barcode_path }}    
+            </h2>
+        <div>
+            {{ $card->barcode_path }}
+        </div>
+        
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
